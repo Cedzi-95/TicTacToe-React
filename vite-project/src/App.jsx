4e04320f2f7c36b3fcfc159fd6ce1dc96be741da
Cedzi-95 <cedricmasinzo@yahoo.com>
2025-03-14 15:41:23 +0100

    add usestate

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Square from './game'
+// import Square from './game'
+import Board from './game'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Square/>
+     <Board/>
       
     </>
   )
